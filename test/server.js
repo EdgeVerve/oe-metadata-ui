f1a59f1f52a1c781485eb0ff779731d8cd89b98c
kvsrohit <kvsrohit@yahoo.com>
2018-08-02 09:16:20 +0530

    remove unnecessary packages

--- test/server.js
+++ test/server.js
@@ -1,6 +1,5 @@
 var oecloud = require('oe-cloud');
 var loopback=require('loopback');
-oecloud.attachMixinsToBaseEntity("SkeletonMixin");
 
 oecloud.observe('loaded', function (ctx, next) {
   return next();
